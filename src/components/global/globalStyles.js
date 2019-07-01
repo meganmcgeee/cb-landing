188df70802f15fea6b9d54f8d0ba898eac8de4a5
kieranstartup <info@kieranstartup.co.uk>
2019-07-01 11:25:17 +0100

    building scaffolding

---
+++ src/components/global/globalStyles.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+body {
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,  Arial, sans-serif;
+  font-weight: 300;
+
+
+  font-size: 16px;
+  line-height: 1.5;
+  letter-spacing: 0.6px;
+
+  font-weight: normal;
+  text-shadow: 1px 1px 1px rgba(0, 0, 0, .004);
+  text-rendering: optimizeLegibility !important;
+  -webkit-font-smoothing: antialiased !important;
+  -webkit-tap-highlight-color: transparent;
+
+  color: #404040;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-weight: 300;
+  margin: 0;
+}
+
+video,
+img {
+  width: 100%;
+}
+
+/*--------------------------------------------------------------
+  ## Links
+  --------------------------------------------------------------*/
+a {
+  color: #404040;
+  text-decoration: none;
+}
+
+a:visited {
+  color: #404040;
+}
+
+a:hover,
+a:focus,
+a:active {
+  color: #404040;
+}
+
+a:hover,
+a:active,
+a:focus {
+  outline: 0;
+}
+
+/*--------------------------------------------------------------
+  ## Media Queries
+  --------------------------------------------------------------*/
+
+@media (max-width: 992px) {
+  h1,
+  h2 {
+    font-size: 20px;
+  }
+}
+
+`;
+
+export default GlobalStyle;
--- src/components/global/layout.js
+++ src/components/global/layout.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import {
+  BaseCSS,
+  Container,
+  GridThemeProvider,
+  Row,
+} from 'styled-bootstrap-grid';
+import { Normalize } from 'styled-normalize';
+import GlobalStyles from './globalStyles';
+
+const gridTheme = {
+  breakpoints: {
+    // defaults below
+    xl: 1200,
+    lg: 992,
+    md: 768,
+    sm: 576,
+    xs: 575,
+    // or you can use aliases
+    // giant: 1200,
+    // desktop: 992,
+    // tablet: 768,
+    // phone: 576,
+    // smaller: 575,
+  },
+  row: {
+    padding: 10, // default 15
+  },
+  col: {
+    padding: 5, // default 15
+  },
+  container: {
+    padding: 0, // default 15
+    maxWidth: {
+      // defaults below
+      xl: 1140,
+      lg: 960,
+      md: 720,
+      sm: 540,
+      xs: 540,
+      // or you can use aliases
+      // giant: 1140,
+      // desktop: 960,
+      // tablet: 720,
+      // phone: 540,
+      // smaller: 540,
+    },
+  },
+};
 
 export default ({ children }) => (
-  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
-    {children}
-  </div>
+  <GridThemeProvider gridTheme={gridTheme}>
+    <Container fluid>
+      <Normalize />
+      <Row>{children}</Row>
+    </Container>
+  </GridThemeProvider>
 );
