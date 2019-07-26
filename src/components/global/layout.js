1e429f77b44a1183492b874a0599a09ba1412c88
kieranstartup <info@kieranstartup.co.uk>
2019-07-26 12:58:10 +0100

    removing react-media as it is not needed

--- src/components/global/layout.js
+++ src/components/global/layout.js
@@ -10,41 +10,21 @@ import {
 import { Normalize } from 'styled-normalize';
 import GlobalStyles from './globalStyles';
 import Helmet from 'react-helmet';
-import Media from 'react-media';
+import styled from 'styled-components';
 import { Location } from '@reach/router';
 import SentenceCase from 'sentence-case';
 
 import Header from './header';
 
-let currentTheme;
+const ContainerWrapper = styled.div`
+  padding: 0 30px;
+
+  @media (max-width: 767px) {
+    padding: 0;
+  }
+`;
 
 const gridTheme = {
-  breakpoints: {
-    xl: 1200,
-    lg: 992,
-    md: 768,
-    sm: 576,
-    xs: 575,
-  },
-  row: {
-    padding: 20,
-  },
-  col: {
-    padding: 20,
-  },
-  container: {
-    padding: 60,
-    maxWidth: {
-      xl: 1140,
-      lg: 960,
-      md: 720,
-      sm: 540,
-      xs: 540,
-    },
-  },
-};
-
-const mobileGridTheme = {
   breakpoints: {
     xl: 1200,
     lg: 992,
@@ -93,83 +73,70 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <React.Fragment>
+      <ContainerWrapper>
         <link rel="stylesheet" href="https://use.typekit.net/epj1ibd.css" />
         <BaseCSS />
         <Normalize />
         <GlobalStyles />
-        <Media query="(max-width: 767px)">
-          {matches => {
-            currentTheme = matches ? mobileGridTheme : gridTheme;
+        <GridThemeProvider gridTheme={gridTheme}>
+          <Container fluid>
+            <Location>
+              {({ location }) => {
+                let title;
 
-            console.log(currentTheme);
-            return (
-              <GridThemeProvider gridTheme={currentTheme}>
-                <Container fluid>
-                  <Location>
-                    {({ location }) => {
-                      let title;
+                if (location.pathname === '/') {
+                  title = `${data.site.siteMetadata.title} – Projects`;
+                } else {
+                  title =
+                    data.site.siteMetadata.title +
+                    ' – ' +
+                    SentenceCase(location.pathname.replace('/', ''));
+                }
 
-                      if (location.pathname === '/') {
-                        title = `${data.site.siteMetadata.title} – Projects`;
-                      } else {
-                        title =
-                          data.site.siteMetadata.title +
-                          ' – ' +
-                          SentenceCase(location.pathname.replace('/', ''));
+                return (
+                  <Helmet>
+                    <title>{title}</title>
+                    <meta name="title" content={title} />
+                    <meta
+                      name="description"
+                      content={data.prismicHome.data.text.text}
+                    />
+                    <meta
+                      property="og:url"
+                      content={
+                        'https://www.carolineboseley.com' + location.pathname
                       }
+                    />
+                    <meta
+                      property="og:description"
+                      content={data.prismicHome.data.text.text}
+                    />
+                    <meta property="og:locale" content="en" />
+                    <meta name="twitter:title" content={title} />
+                    <meta
+                      name="twitter:description"
+                      content={data.prismicHome.data.text.text}
+                    />
+                    <meta name="twitter:card" content="summary_large_image" />
+                  </Helmet>
+                );
+              }}
+            </Location>
 
-                      return (
-                        <Helmet>
-                          <title>{title}</title>
-                          <meta name="title" content={title} />
-                          <meta
-                            name="description"
-                            content={data.prismicHome.data.text.text}
-                          />
-                          <meta
-                            property="og:url"
-                            content={
-                              'https://www.carolineboseley.com' +
-                              location.pathname
-                            }
-                          />
-                          <meta
-                            property="og:description"
-                            content={data.prismicHome.data.text.text}
-                          />
-                          <meta property="og:locale" content="en" />
-                          <meta name="twitter:title" content={title} />
-                          <meta
-                            name="twitter:description"
-                            content={data.prismicHome.data.text.text}
-                          />
-                          <meta
-                            name="twitter:card"
-                            content="summary_large_image"
-                          />
-                        </Helmet>
-                      );
-                    }}
-                  </Location>
-
-                  <Row>
-                    <Col col={12}>
-                      <Header
-                        menuLinks={data.site.siteMetadata.menuLinks}
-                        siteTitle={data.site.siteMetadata.title}
-                      />
-                    </Col>
-                  </Row>
-                  <Row css={{ paddingBottom: '30px' }}>
-                    <Col col={12}>{children}</Col>
-                  </Row>
-                </Container>
-              </GridThemeProvider>
-            );
-          }}
-        </Media>
-      </React.Fragment>
+            <Row>
+              <Col col={12}>
+                <Header
+                  menuLinks={data.site.siteMetadata.menuLinks}
+                  siteTitle={data.site.siteMetadata.title}
+                />
+              </Col>
+            </Row>
+            <Row css={{ paddingBottom: '30px' }}>
+              <Col col={12}>{children}</Col>
+            </Row>
+          </Container>
+        </GridThemeProvider>
+      </ContainerWrapper>
     )}
   />
 );
