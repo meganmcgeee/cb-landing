cef0591955a777c7d3fd2bb2938d68c0571a7705
Mac <machooper9@gmail.com>
2020-12-30 11:31:33 +0000

    about layout done

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,54 +4,15 @@ import { graphql } from 'gatsby';
 import chooseRandomColor from '../components/utils/chooseRandomColor';
 
 import { Row, Col } from 'styled-bootstrap-grid';
-import styled from 'styled-components';
 
 import TextBox from '../components/text/textbox';
 
-const Links = styled.div`
-  a:first-of-type {
-    padding-right: 10px;
-  }
-`;
-
-const SingleLink = styled.a`
-  &:hover {
-    // color: ${props => props.color};
-    color: #404040;
-  }
-`;
-
 const About = ({ data }) => {
   let allColors = ['#6a8493', '#8a432e', '#B1B2B5'];
-
-  const socialLinks = data.prismicHome.data.links.map((link, index) => {
-    return (
-      <SingleLink
-        // color={chooseRandomColor(allColors)}
-        href={link.link.url}
-        target="_blank"
-        rel="noopener noreferrer"
-        key={index}
-      >
-        {link.link_title}
-      </SingleLink>
-    );
-  });
-
   return (
     <Layout>
       <Row>
-        <Col col={12} sm={6} md={5} lg={4} xl={3}>
-          <img src={data.prismicAbout.data.image.url} />
-        </Col>
-      </Row>
-      <Row>
         <Col col={12} sm={10} md={8} lg={6}>
-          <TextBox
-            text={data.prismicHome.data.contact_text}
-            margin={'20px 0 1em 0'}
-          />
-          <Links>{socialLinks}</Links>
           <TextBox text={data.prismicAbout.data.text} margin={'1em 0 '} />
         </Col>
       </Row>
