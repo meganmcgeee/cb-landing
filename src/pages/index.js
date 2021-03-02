eb5ec68e900fa689732d8a83b334e5cb1bc1095d
Mac <machooper9@gmail.com>
2021-03-02 16:54:29 +0000

    homepage done

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,11 @@ export default function Index() {
     <>
     <Link to="/projects">
       <div className="container">
-        <svg id="line" width="100vh" height="100vh">
-          <path id="linepath" d="M 0 60 C 180 30, 75 15, 190 305 S 500 -150, 950 300" stroke="white" fill="transparent"/>
+        <svg id="line" width="10000px" height="10000px">
+          <path id="linepath" d="M 0 200 C 100 130, 105 100, 270 615 S 690 -250, 1400 250" stroke="white" fill="transparent"/>
         </svg>
         <div className="content">
-          <p><strong>Caroline Boseley</strong> is an independent</p> <p>curator whose practice explores the role of <i>contemporary art</i> in the <i>public realm</i>. She explores how art outside of the gallery creates <i>space of encounter</i> and <i>community engagement</i>.</p>
+          <p><strong>Caroline Boseley</strong> is an independent</p> <p>curator whose practice explores the role of <br className="desktop-break"/> contemporary art in the public realm. She explores <br className="desktop-break"/>how art outside of the gallery creates space of <br className="desktop-break"/> encounter and community engagement.</p>
         </div>
       </div>
     </Link>
@@ -22,16 +22,17 @@ export default function Index() {
 	      src: url("https://s3-eu-west-1.amazonaws.com/fonts-ireland.typotheque.com/WF-033325-010774-000275-e85a17905b3816b4f58536ce5a1555f8.eot?#iefix") format("embedded-opentype"), url("https://s3-eu-west-1.amazonaws.com/fonts-ireland.typotheque.com/WF-033325-010774-000275-e85a17905b3816b4f58536ce5a1555f8.woff2") format("woff2"), url("https://s3-eu-west-1.amazonaws.com/fonts-ireland.typotheque.com/WF-033325-010774-000275-e85a17905b3816b4f58536ce5a1555f8.woff") format("woff"), url("https://s3-eu-west-1.amazonaws.com/fonts-ireland.typotheque.com/WF-033325-010774-000275-e85a17905b3816b4f58536ce5a1555f8.svg#typotheque_webfonts_service") format("svg");
       }
       html, body {
-        background: rgb(24, 22, 42);
+        background: rgb(13, 13, 26);
         color: #B1B2B5;
-        font-size: 45px;
-        font-weight: thin;
+        font-family: akzidenz-grotesk-pro,-apple-system,system-ui,sans-serif;
+        font-size: 38px;
       }
       .container {
         width: 100vw;
-        height: 75vw;
-        display: grid;
-        place-items: center;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
       }
       .container #line {
         position: absolute;
@@ -48,20 +49,42 @@ export default function Index() {
         text-decoration: none;
         color: inherit;
       }
+      svg {
+        display: none;
+      }
       .content p:nth-child(2) {
         max-width: 1000px;
       }
       .content strong {
             font-family: 'History 01';
-            font-size: 30px;
-            font-weight: 600;
+            font-size: 60px;
             font-smooth: antialiased;
             -webkit-font-smoothing: antialiased;
             -moz-osx-font-smoothing: antialiased;
       }
+      .desktop-break {
+        display: none;
+      }
         @media(min-width: 760px) {
-          .content strong {
+          .container {
+            justify-content: flex-end;
+          }
+          .content {
+            margin: 0 auto;
             font-size: 60px;
+          }
+          .content strong {
+            font-size: 90px;
+          }
+          .content p, .content p:nth-child(2) {
+            width: 80vw;
+            max-width: 2800px;
+          }
+          svg {
+            display: block;
+          }
+          .desktop-break {
+            display: block;
           }
         }
       `}</style>
